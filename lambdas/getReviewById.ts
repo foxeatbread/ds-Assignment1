a16cd5a79f22eb59b433160be12cdcfaacc34ad6
顾昊轩 <guhaoxuan@guhaoxuandeMacBook-Pro.local>
2024-03-04 12:23:07 +0000

    GET /movies/{movieId}/reviews?minRating=n - Get the reviews for the specified movie with a rating greater than the minRating.

--- lambdas/getReviewById.ts
+++ lambdas/getReviewById.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand,GetCommand } from "@aws-sdk/lib-dynamodb";
 
 const ddbDocClient = createDDbDocClient();
 
@@ -8,8 +8,8 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
     try {
         console.log("Event: ", event);
         const parameters = event?.pathParameters;
+        const queryParams = event.queryStringParameters;
         const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
-
         if (!movieId) {
             return {
                 statusCode: 404,
@@ -20,32 +20,70 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
             };
         }
 
-        // Query reviews for the specified movieId
-        const commandOutput = await ddbDocClient.send(
-            new QueryCommand({
-                TableName: process.env.TABLE_NAME,
-                KeyConditionExpression: "movieId = :id",
-                ExpressionAttributeValues: {
-                    ":id": movieId,
-                },
-            })
-        );
+        let responseData: any = null;
 
-        const reviews = commandOutput.Items;
+        if (queryParams && queryParams.minRating) {
+            const minRating = parseInt(queryParams.minRating); // 将查询参数转换为整数
+            if (!isNaN(minRating) && minRating >= 1 && minRating <= 5) {
+                const commandInput = {
+                    TableName: process.env.TABLE_NAME,
+                    KeyConditionExpression: "movieId = :m",
+                    FilterExpression: "rating > :mr",
+                    ExpressionAttributeValues: {
+                        ":m": movieId,
+                        ":mr": minRating
+                    }
+                }
+            const commandOutput = await ddbDocClient.send(new QueryCommand(commandInput))
+            const reviews = commandOutput.Items;
 
-        if (!reviews || reviews.length === 0) {
-            return {
-                statusCode: 404,
-                headers: {
-                    "content-type": "application/json",
-                },
-                body: JSON.stringify({ Message: "No reviews found for the specified movie Id" }),
+            if (!reviews || reviews.length === 0) {
+                return {
+                    statusCode: 404,
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                    body: JSON.stringify({ Message: "No reviews found for the specified movie Id" }),
+                };
+            }
+
+            responseData = {
+                reviews,
+            };
+            } else {
+                console.error("Invalid minRating parameter");
+            }
+
+
+        } else {
+            // Query reviews for the specified movieId
+            const commandOutput = await ddbDocClient.send(
+                new QueryCommand({
+                    TableName: process.env.TABLE_NAME,
+                    KeyConditionExpression: "movieId = :id",
+                    ExpressionAttributeValues: {
+                        ":id": movieId,
+                    },
+                })
+            );
+
+            const reviews = commandOutput.Items;
+
+            if (!reviews || reviews.length === 0) {
+                return {
+                    statusCode: 404,
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                    body: JSON.stringify({ Message: "No reviews found for the specified movie Id" }),
+                };
+            }
+
+            responseData = {
+                reviews,
             };
         }
 
-        const body = {
-            data: reviews,
-        };
 
         // Return Response
         return {
@@ -53,7 +91,7 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
             headers: {
                 "content-type": "application/json",
             },
-            body: JSON.stringify(body),
+            body: JSON.stringify(responseData),
         };
     } catch (error: any) {
         console.log(JSON.stringify(error));
